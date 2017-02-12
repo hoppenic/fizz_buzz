e29ca1360068935761c489aca294c4189e5a7025
Nick Hoppe <hoppenic@gmail.com>
2017-02-12 01:33:51 +0000

    updated files

--- Int_Test.py
+++ Int_Test.py
@@ -1,16 +1,24 @@
 
 
-#test script using the int  function
+#test script using the int function
+#did this differently than what the lesson taught but it worked as I couldn't figure out the other way
 
 
 
-user_input=input("please enter a number 1 through 10")
+user_input=input("Please enter a number 1 through 10")
 
-user_input=int(user_input)
-
-
-for n in range(0,user_input):
-    print(user_input)
+    
+for n in range(0,int(user_input)):
+    if n % 15 == 0:
+        print("fizzbuzz")
+    elif n % 3 == 0:
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
+    else:
+        print(n)   
+    
+    
     
     
     
--- fizzbuzzExtra.py
+++ fizzbuzzExtra.py
@@ -1,13 +1,14 @@
 # making a more advanced fizzbuzz program 
 # if user supplies inputs on command line, use that, else use the input function to obtain their input and run through the fizzbuzz loop
 # have to switch the user input from a string to integer
+# Converting str to int gave me trouble, it didn't seem to work like the thinkful text said it would
 
 import sys
 
 print("The name of this script is {}".format(sys.argv[0]))
 print("User supplied {} arguments at run time".format(len(sys.argv)))
 
-if 
+
 
 for arg in sys.argv[1:]:
     if int(arg) % 15 == 0:
@@ -19,14 +20,13 @@ for arg in sys.argv[1:]:
     else:
       print(arg)
   
-else:
+
     
 user_input=input("Please enter a number between 1 and 50")
 
-user_input=int(user_input) #why did I have to do it this way for the second half of this?  int(n) did not work
 
 
-for n in range(0,user_input):
+for n in range(0,int(user_input)):
     if n % 15 == 0:
         print("fizzbuzz")
     elif n % 3 == 0:
